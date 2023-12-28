0678022fc66a2850f3496c9d6f96a191c2425304
NazimLeeman <nazimleeman@gmail.com>
2023-12-28 10:05:44 +0600

    deployment testing

--- server/src/config.ts
+++ server/src/config.ts
@@ -4,7 +4,8 @@ dotenv.config();
 const config = {
     PORT: process.env.PORT ?? 4000,
     CORS_ORIGIN: process.env.CORS_ORIGIN ?? "*",
-    DB_URI: process.env.DB_URI ?? 'postgres://postgres:12345@localhost:5432/hrdb'
+    // DB_URI: process.env.DB_URI ?? 'postgres://postgres:12345@localhost:5432/hrdb'
+    DB_URI: process.env.DB_URI ?? 'postgres://koyeb-adm:y67uAqbPRCTe@ep-lingering-rain-63477638.eu-central-1.aws.neon.tech/koyebdb'
 }
 
 export default config;
--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,6 +1,13 @@
 import { Sequelize } from 'sequelize';
 import config from '../config';
 
-const sequelize = new Sequelize(config.DB_URI);
+const sequelize = new Sequelize(config.DB_URI, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // You may need to set this to false in some environments
+      },
+    },
+  });
 
 export default sequelize;
