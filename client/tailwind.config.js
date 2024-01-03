4879429b1f7f6fd187bc2815e4371112eecd293b
Nazim Leeman <nazimleeman@gmail.com>
2024-01-03 14:21:31 +0600

    fixed tw for signup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
+    // screens: {
+    //   sm: '480px',
+    //   md: '768px',
+    //   lg: '976px',
+    //   xl: '1440px',
+    // },
     // colors: {
     //   'dark-green': '#02643B',
     //   'green': '#038851',
