8126f8962f3485d2d81cd0e18ba5ac826a5a7ab0
Nazim Leeman <nazimleeman@gmail.com>
2024-01-03 22:57:17 +0600

    applicant side tw fixed

--- client/src/app/skills/skills.component.ts
+++ client/src/app/skills/skills.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, NonNullableFormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ApiClientService } from '../api-client.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-skills',
@@ -9,23 +10,42 @@ import { ApiClientService } from '../api-client.service';
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
+  applicantId: number = 0;
+  skillTags: string = '';
+  showSkillTagsForm: boolean = false;
   validateForm: FormGroup<{
     skillTags: FormControl<string>;
   }>
 
-  submitForm(): void {
-    if (this.validateForm.valid) {
-      const userData = this.validateForm.value;
-      this.apiClientService.registerUser(userData).subscribe((response) => {
-        console.log('Applicant registered successfully:', response);
-        // this.router.navigate(['/success'])
-        this.router.navigate(['/summary']);
-        // this.showSuccessNotification();
+  ngOnInit(): void {
+    this.route.params.pipe(
+      switchMap((params) => {
+          this.applicantId = params['applicantId'];
+          return this.apiClientService.getApplicantData(this.applicantId);
+      })
+  ).subscribe(
+      (data: any) => {
+        console.log('API Response:', data);
+      this.skillTags = data.data.skillTags;
       },
       (error) => {
-        console.log("Error during resgistration", error)
+          console.error('Error fetching data from the API', error);
+      }
+    );
+  }
+
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      const updatedData = this.validateForm.value;
+      console.log('merged data:', updatedData)
+      // const updatedData = this.validateForm.value;
+      this.apiClientService.updateApplicantData(this.applicantId, updatedData).subscribe((response) => {
+        console.log('Applicant updated successfully:', response);
+        location.reload();
+      },
+      (error) => {
+        console.log("Error during update", error)
       })
-      // console.log('submit', this.validateForm.value);
     } else {
       Object.values(this.validateForm.controls).forEach(control => {
         if (control.invalid) {
@@ -36,7 +56,11 @@ export class SkillsComponent {
     }
   }
 
-  constructor(private fb: NonNullableFormBuilder, private apiClientService: ApiClientService, private router: Router) {
+  toggleExperienceForm(): void {
+    this.showSkillTagsForm = !this.showSkillTagsForm;
+  }
+
+  constructor(private fb: NonNullableFormBuilder, private apiClientService: ApiClientService, private router: Router, private route: ActivatedRoute) {
     this.validateForm = this.fb.group({
       skillTags: ['', [Validators.required]],
     })
