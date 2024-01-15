ca3ec5078de89e187cec51596fd324afe7afa3ad
NazimLeeman <nazimleeman@gmail.com>
2024-01-15 16:45:00 +0600

    employee pic

--- client/src/app/graph/graph.component.ts
+++ client/src/app/graph/graph.component.ts
@@ -16,7 +16,7 @@ export class GraphComponent {
       x: {
         ticks: {
           font: {
-            size: 40,
+            size: 32,
           },
         },
       },
@@ -24,7 +24,7 @@ export class GraphComponent {
         min: 10,
         ticks: {
           font: {
-            size: 40, 
+            size: 32, 
           },
         },  
       },
@@ -37,7 +37,7 @@ export class GraphComponent {
         anchor: 'end',
         align: 'end',
         font: {
-          size: 40,
+          size: 32,
         }
       } ,
     },
@@ -46,14 +46,12 @@ export class GraphComponent {
   public barChartPlugins = [DataLabelsPlugin];
 
   public barChartData: ChartData<'bar'> = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    labels: ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY'],
     datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+      { data: [12, 16, 19, 15, 11, 12, 20], label: 'Series A' },
+      { data: [12, 16, 19, 15, 11, 12, 21], label: 'Series B' },
     ],
   };
-
-  // events
   public chartClicked({
     event,
     active,
@@ -75,7 +73,6 @@ export class GraphComponent {
   }
 
   public randomize(): void {
-    // Only Change 3 values
     this.barChartData.datasets[0].data = [
       Math.round(Math.random() * 100),
       59,
