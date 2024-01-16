54dc0b58b8d67cb44486c4e878e530c5cc84f428
NazimLeeman <nazimleeman@gmail.com>
2024-01-16 12:06:46 +0600

    set address

--- client/src/app/address/address.component.ts
+++ client/src/app/address/address.component.ts
@@ -8,55 +8,55 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrl: './address.component.css'
 })
 export class AddressComponent {
-  // addressForm: FormGroup;
+  addressForm: FormGroup;
 
-  // constructor(
-  //   private router: Router,
-  //   private formBuilder: FormBuilder
-  // ) {
-  //   this.addressForm = this.formBuilder.group({
-  //     address: ['', [Validators.required]],
-  //   });
-  // }
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {
+    this.addressForm = this.formBuilder.group({
+      address: ['', [Validators.required]],
+    });
+  }
 
-  // @ViewChild('mapSearchField') searchField: ElementRef | undefined;
-  // ngAfterViewInit(): void {
-  //   const searchBox = new google.maps.places.SearchBox(
-  //     this.searchField?.nativeElement
-  //   );
+  @ViewChild('mapSearchField') searchField: ElementRef | undefined;
+  ngAfterViewInit(): void {
+    const searchBox = new google.maps.places.SearchBox(
+      this.searchField?.nativeElement
+    );
 
-  //   searchBox.addListener('places_changed', () => {
-  //     const places = searchBox.getPlaces();
-  //     if (places?.length === 0) {
-  //       return;
-  //     }
-  //     places?.forEach((place:any) => {
-  //       console.log(place.name, place.address_components);
-  //       console.log(
-  //         place.geometry?.location?.lat(),
-  //         place.geometry?.location?.lng()
-  //       );
-  //     });
-  //   });
-  // }
-  // /**
-  //  * Checks if a form field is invalid based on its name.
-  //  * @param fieldName The name of the form field to check.
-  //  * @returns True if the field is invalid and has been touched or modified, false otherwise.
-  //  */
-  // isFieldInvalid(fieldName: string): boolean {
-  //   const fieldControl = this.addressForm.get(fieldName);
-  //   if (fieldControl) {
-  //     return (
-  //       fieldControl.invalid && (fieldControl.dirty || fieldControl.touched)
-  //     );
-  //   }
-  //   return false;
-  // }
+    searchBox.addListener('places_changed', () => {
+      const places = searchBox.getPlaces();
+      if (places?.length === 0) {
+        return;
+      }
+      places?.forEach((place:any) => {
+        console.log(place.name, place.address_components);
+        console.log(
+          place.geometry?.location?.lat(),
+          place.geometry?.location?.lng()
+        );
+      });
+    });
+  }
+  /**
+   * Checks if a form field is invalid based on its name.
+   * @param fieldName The name of the form field to check.
+   * @returns True if the field is invalid and has been touched or modified, false otherwise.
+   */
+  isFieldInvalid(fieldName: string): boolean {
+    const fieldControl = this.addressForm.get(fieldName);
+    if (fieldControl) {
+      return (
+        fieldControl.invalid && (fieldControl.dirty || fieldControl.touched)
+      );
+    }
+    return false;
+  }
 
-  // handleLoginClick(): void {
-  //   if (this.addressForm.valid) {
-  //     const loginData = this.addressForm.value;
-  //   }
-  // }
+  handleLoginClick(): void {
+    if (this.addressForm.valid) {
+      const loginData = this.addressForm.value;
+    }
+  }
 }
