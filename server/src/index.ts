47f575a8807e350f079439cbafef5c7d52106b5d
NazimLeeman <nazimleeman@gmail.com>
2024-01-23 19:33:13 +0600

    fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,7 @@ import authRouter from './routers/auth.router';
 const app: Express = express();
 
 app.use(cors({
-  origin: config.CORS_ORIGIN
+  origin: config.CORS_ORIGIN.split(',')
 }));
 
 app.use(express.json());
